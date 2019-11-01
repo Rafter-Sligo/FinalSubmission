5db88c881ee47629ee87b2aecfccd679ce811707
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-01 23:24:14 +0000

    working on the ingredient and shopping-list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class ingredient {
+    name: string;
+    amount: number;
+
+    constructor(name:string, amount:number){
+        this.name = name;
+        this.amount = amount
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient} from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients : ingredient[] = [
+    new ingredient('Cut Beef', 200),
+    new ingredient('Onion', 10)
+
+  ];
 
   constructor() { }
 
