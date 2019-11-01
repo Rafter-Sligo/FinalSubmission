2e35fb58fad77b2c5ff8a0de21c29e43cdee46b2
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-01 22:22:35 +0000

    Recipe Details

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this desc',
+    'https://www.thespruceeats.com/thmb/hl4lkmdLO7tj1eDCsGbakfk97Co=/3088x2055/filters:fill(auto,1)/marinated-top-round-steak-3060302-hero-02-ed071d5d7e584bea82857112aa734a94.jpg')
+  ];
+
+  
   constructor() { }
+
 
   ngOnInit() {
   }
--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,9 +1,8 @@
 export class Recipe{
-    public name: string;
-    public description: string;
-    public imgPath: string;
-
-
+    name: string;
+    description: string;
+    imgPath: string;
+    
     constructor(name: string, desc: string, imagePath:string){
         this.name = name;
         this.description = desc;
