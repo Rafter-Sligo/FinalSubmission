5f1a0899086acc2f53f284f08c3bff6a013017cb
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-02 14:59:25 +0000

    Working on the Recipe Service

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  styleUrls: ['./recipe.component.css'],
+  providers: [RecipeService]
 })
 export class RecipeComponent implements OnInit {
   selectedRecipe: Recipe
