05404b321568199facb81c126cdeeb740469f9bc
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-03 11:06:47 +0000

    Done some Routing, Broke the recipe-Item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
 
 //The Routes
 const appRoute: Routes = [
     //pathMatch will display went the path is empty
     {path: '',redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipeComponent, children: [
-        { path: ''}
+        { path: '',component: RecipeStartComponent} //making it display inside the recipe component
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 
