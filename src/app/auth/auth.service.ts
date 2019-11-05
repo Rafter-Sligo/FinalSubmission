8d0e6870f5f4e7e68768d53ff22376b972161123
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-05 22:42:12 +0000

    Working with Users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData{
     kind: string;
@@ -15,6 +16,9 @@ export interface AuthResponseData{
 
 @Injectable({providedIn:'root'})
 export class AuthService{
+
+    user = new Subject<User>();
+
 
     constructor(private http: HttpClient){
 
@@ -30,7 +34,16 @@ export class AuthService{
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), 
+        tap(resData =>{
+            this.handAuthentication(
+                resData.email,
+                resData.localId,
+                resData.idToken,
+                +resData.expiresIn
+                );
+        })      
+        );
     }
 
 
@@ -43,8 +56,30 @@ export class AuthService{
                 password: password,
                 returnSecureToken: true            
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), 
+        tap(resData =>{
+            this.handAuthentication(
+                resData.email,
+                resData.localId,
+                resData.idToken,
+                +resData.expiresIn
+                );
+        })
+        );
     }
+
+    private handAuthentication(email: string, userId: string, token: string, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);  // returns a date object in milliseconds
+        const user = new User(
+            email,
+            userId ,
+            token,
+            expirationDate
+            );
+        this.user.next(user);    
+
+    }
+
 
     private handleError(errorRes: HttpErrorResponse)
         {
