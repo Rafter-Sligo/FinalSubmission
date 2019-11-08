89cbe5a5b938960f3446cbfc6008d8dd9de66486
SligosFinest <44242819+SligosFinest@users.noreply.github.com>
2019-11-08 03:34:58 +0000

    calling API

--- src/app/newest-recipe/newest-recipe.component.ts
+++ src/app/newest-recipe/newest-recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewRecipe } from './newest-recipe.model';
+import { NewRecipeService } from './newest-recipe.service';
 
 @Component({
   selector: 'app-newest-recipe',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewestRecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private newRecipeService : NewRecipeService){
 
+  }
+  
   ngOnInit() {
+  }
+
+  recipeData: NewRecipe;
+  errorMessage:any;
+  imageHeight=300;
+
+
+  getRecipeDetails(recipeName: string):boolean{
+    this.newRecipeService.getRecipeData(recipeName).subscribe(Data =>{
+      this.recipeData = Data;
+    },
+
+    error => this.errorMessage = <any>error);
+    return false;
   }
 
 }
